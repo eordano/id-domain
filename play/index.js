87e66e9f21f069a1e8d3536a4e1b2d2a2205ff32
Esteban Ordano <esteban@decentraland.org>
2023-02-06 00:09:04 -0300

    initial commit

---
+++ play/index.js
@@ -0,0 +1,75 @@
+;(function($, doc) {
+
+  const callbacksById = new Map()
+  let source = null
+  const untilSourceIsReady = new Promise((resolve) => {
+    $.addEventListener('message', function(event) {
+      if (event.origin !== 'https://id.decentraland.org') {
+        return
+      }
+      if (!source) {
+        source = event.source
+        resolve(source)
+      } else {
+        const data = JSON.parse(event.data)
+        if (!data.id) {
+          console.error('eps')
+          return
+        }
+        const callback = callbacksById.get(data.id)
+        if (callback) {
+          callback(data)
+        }
+      }
+    }, false)
+  })
+
+  const frame = doc.createElement("IFRAME"); 
+  frame.setAttribute("src", "https://id.decentraland.org/?play");
+  doc.body.appendChild(frame);
+
+  $.idGet = function(key) {
+    const id = Math.random().toFixed(8).substring(2)
+    const result = new Promise(async (resolve, reject) => {
+      callbacksById.set(id, function(result) {
+        if (result.status === 'ok') {
+          return resolve(result.value)
+        } else {
+          return reject(result)
+        }
+      })
+      if (!source) {
+        await untilSourceIsReady
+      }
+      source.postMessage(JSON.stringify({
+        type: 'get',
+        id,
+        key
+      }), 'https://id.decentraland.org')
+    })
+    return result
+  }
+  $.idSet = function(key, value) {
+    const id = Math.random().toFixed(8).substring(2)
+    const result = new Promise(async (resolve, reject) => {
+      callbacksById.set(id, function(result) {
+        if (result.status === 'ok') {
+          return resolve(result.value)
+        } else {
+          return reject(result)
+        }
+      })
+      if (!source) {
+        await untilSourceIsReady
+      }
+      source.postMessage(JSON.stringify({
+        type: 'set',
+        id,
+        key,
+        value
+      }), 'https://id.decentraland.org')
+    })
+    return result
+  }
+
+})(window, window.document)
